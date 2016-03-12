ee61e512f61cc3b2c817d3482a7f96ae054a790b
Adam Sagoskin <asagoski@ycp.edu>
2016-03-12 17:37:31 -0500

    updated

--- CS320_Lab03/main/edu/ycp/cs320/lab03/main/Main.java
+++ CS320_Lab03/main/edu/ycp/cs320/lab03/main/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
 		// Create and register a webapp context
 		WebAppContext handler = new WebAppContext();
-		handler.setContextPath("/lab03");
+		handler.setContextPath("/HotelReservationSystem");
 		handler.setWar("./war"); // web app is in the war directory of the project
 		server.setHandler(handler);
 		
