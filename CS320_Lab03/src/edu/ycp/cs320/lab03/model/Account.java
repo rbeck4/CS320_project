b66295330e4c82af3bbd1b662e7d75725fbf4f06
Adam Sagoskin <asagoski@ycp.edu>
2016-03-17 10:15:21 -0400

    updated account java class model

--- CS320_Lab03/src/edu/ycp/cs320/lab03/model/Account.java
+++ CS320_Lab03/src/edu/ycp/cs320/lab03/model/Account.java
@@ -39,6 +39,26 @@ public class Account {
 	public boolean validTest() {
 		return true;
 	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
+	public String getPassword() {
+		return password;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
 
 }
