7a25f6d7501c5240a30883c6a6992de23fb4f05b
Adam Sagoskin <asagoski@ycp.edu>
2016-03-30 17:09:40 -0400

    Updated webScraper

--- CS320_Lab03/src/edu/ycp/cs320/lab03/controller/SearchRequestController.java
+++ CS320_Lab03/src/edu/ycp/cs320/lab03/controller/SearchRequestController.java
@@ -1,6 +1,7 @@
 package edu.ycp.cs320.lab03.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -37,19 +38,32 @@ public class SearchRequestController {
 		      //  System.out.println("text: " + link.text());
 		      //}
 			  //Elements hotels = doc.select(#);
-		      Elements links = doc.select("a.hotel_name_link.url");
+		      Elements hotels = doc.select("a.hotel_name_link.url");
 		      Elements prices = doc.select("b");
 		      Elements rooms = doc.select("span.room_link");
-		      for (Element link : links) {
+		      
+		      System.out.println("First Hotel: " +hotels.get(0).text());
+		      System.out.println("First Hotel: " + prices.get(0).text());
+		      
+		      ArrayList<String> hotelNames = new ArrayList<String>();
+		      hotelNames.add(hotels.get(0).text());
+		      
+		      for(int i = 0; i < 15; i++) {   
+		    	    System.out.println("Hotel: " + hotels.get(i).text());
+		    	    System.out.println("Price: " + prices.get(i).text());
+		    	    System.out.println("Room type: " + rooms.get(i).text());
+		      } 
+		      
+		      //for (Element hotel : hotels) {
 		        //get the value from the href attribute
 		        //System.out.println("\nlink: " + link.attr("href"));
-		        System.out.println("Hotel: " + link.text());
-		      }
+		    	//System.out.println("Hotel: " + link.text());
+		      //}
 		      for (Element price : prices) {
-			    System.out.println("Price: " + price.text());
+			    //System.out.println("Price: " + price.text());
 			  }
 		      for (Element room : rooms) {
-				    System.out.println("Room type: " + room.text());
+				  //  System.out.println("Room type: " + room.text());
 		      }
 			
 		     // Elements xxxDivs = doc.select("div.answer[name=ss]");
@@ -57,7 +71,7 @@ public class SearchRequestController {
 		      
 		      
 		    		  
-		      //Document doc1 = (Document) Jsoup.connect("http://www.booking.com/searchresults.html?label=gen173nr-1FCAEoggJCAlhYSDNiBW5vcmVmcgV1c192YYgBAZgBMbgBDMgBD9gBAegBAfgBAqgCAw;sid=c7ddbaef979b3e3d4c98b522ade5707c;dcid=4;checkin="checkI";checkout="checkO";city=20053799;dcsc=2&")
+		      //Document doc1 = (Document) Jsoup.connect("http://www.booking.com/searchresults.html?label=gen173nr-1FCAEoggJCAlhYSDNiBW5vcmVmcgV1c192YYgBAZgBMbgBDMgBD9gBAegBAfgBAqgCAw&sid=c7ddbaef979b3e3d4c98b522ade5707c&dcid=4&checkin="+checkIn+"&checkout="+checkOut+"&city=20053799&class_interval=1&dcsc=2&dtdisc=0&group_adults=2&group_children=0&hlrd=0&hyb_red=0&inac=0&label_click=undef&nha_red=0&no_rooms=1&postcard=0&redirected_from_city=0&redirected_from_landmark=0&redirected_from_region=0&review_score_group=empty&room1=A%2CA&sb_price_type=total&score_min=0&ss_all=0&ssb=empty&sshis=0&order=score")
 		    		//  .data("ss", "Harrisburg, Pennsylvania, USA")
 		    		  // and other hidden fields which are being passed in post request.
 		    		//  .userAgent("Mozilla")
